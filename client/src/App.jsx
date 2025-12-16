ef216f3353cac4e62bb0618b795bfc715d218231
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-12-17 04:30:22 +0900

    scene of Layout abstraction

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import { createTheme } from "@mui/material/styles"
 import { useMemo } from 'react'
 import { useSelector } from "react-redux"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { themeSettings } from "theme"
+import { themeSettings } from "./theme"
+import Layout from 'scenes/layout'
 import Dashboard from 'scenes/dashboard'
 
 function App() {
